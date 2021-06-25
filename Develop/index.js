d8e91e3a9407e814f80e7453d1689c8311e7b1ef
Dana Merrill <dtmerrilliii@gmail.com>
2021-06-25 19:52:44 -0400

    working version

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,125 @@
-// TODO: Include packages needed for this application
 
-// TODO: Create an array of questions for user input
-const questions = [];
-
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
-
-// TODO: Create a function to initialize app
+//  Function init and call
 function init() {}
-
-// Function call to initialize app
 init();
+const inquirer = require("inquirer");
+const fs = require("fs");
+
+//  Questions
+inquirer
+  .prompt([
+    {
+      type: "input",
+      message: "Please provide a title/headline for this project?",
+      name: "headline",
+    },
+
+    {
+      type: "input",
+      message: "Why are you doing this project?",
+      name: "why",
+    },
+
+    {
+      type: "input",
+      message: "What is interesting about this project?",
+      name: "what",
+    },
+
+    {
+      type: "input",
+      message: "How was the project accomplished? (ex. tech)",
+      name: "how",
+    },
+
+    {
+      type: "input",
+      message: "What are the installation instructions?",
+      name: "instructions",
+    },
+
+    {
+      type: "input",
+      message: "What is the usage information?",
+      name: "information",
+    },
+
+    {
+      type: "input",
+      message: "What are the contribution guidelines?",
+      name: "guidelines",
+    },
+
+    {
+      type: "input",
+      message: "Are there any applicable or relevant tests?",
+      name: "tests",
+    },
+
+    {
+      type: "input",
+      message: "How should project questions be addressed?",
+      name: "questions",
+    },
+
+    {
+      type: "list",
+      message: "Please provide license information:",
+      name: "license",
+      choices: ["MIT", "Licensed (other)", "N/A", "Unlicensed"],
+    },
+
+    {
+      type: "input",
+      message: "Please provide an email address:",
+      name: "email",
+    },
+
+    {
+      type: "input",
+      message: "Please provide a Github username:",
+      name: "github",
+    },
+  ])
+
+//  Layout and Load the ReadMe page
+  .then((data) => {
+    var string = `## Project Headline: ${data.headline}<br><br><br>
+    Table of Contents:  
+[Project Why](#why)<br>
+[Project Designers Contact/Github](#contact)<br><br><br>
+    
+    
+## Why, What and How, Installation, Usage, Contributors, and Testing
+#### (why)
+<br><br>
+Project Why: ${data.why}
+<br>
+Project What: ${data.what}
+<br>
+Project How: ${data.how}
+<br>
+Installation Instructions:  ${data.instructions}
+<br>
+Usage Information: ${data.information}
+<br>
+Guidelines for Contributions to this Project:  ${data.guidelines}
+<br>
+Relevant Tests: ${data.tests}
+<br>
+<br>
+## Contact, Github, Email, License
+#### (contact information)
+<br>
+Questions Regarding this Project and/or its Usage: ${data.questions}
+<br>
+Project License: ${data.license}
+<br>
+Email Project Designer: [${data.email}](${data.email}/)
+<br>
+Designers Github Page: https://github.com/${data.github} `;
+
+    fs.writeFileSync("README.md", string, (err) =>
+      err ? console.log(err) : console.log("write successful")
+    );
+  });
